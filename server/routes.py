0d455ec4061b2db12e92c00d94b6e7e1d2eef311
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2025-02-04 00:16:50 +0100

    test: conflict

--- server/routes.py
+++ server/routes.py
@@ -14,19 +14,19 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+            "SELECT * FROM books WHERE name LIKE '%" + name + "%' "
         )
         books = [Book(*row) for row in cursor]
 
     elif author:
         cursor.execute(
-            "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
+            "SELECT * FROM books WHERE author LIKE '%" + author + "%' "
         )
         books = [Book(*row) for row in cursor]
 
     elif test:
         cursor.execute(
-            "SELECT * FROM books WHERE test LIKE '%" + test + "%'"
+            "SELECT * FROM books WHERE test LIKE '%" + test + "%' "
         )
         books = [Book(*row) for row in cursor]
     else:
